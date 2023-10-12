3f73c71667318d03dd4fdbea0d4c54e62428d411
antoniopelusi <antoniopelusi2000@gmail.com>
2023-10-12 16:16:39 +0200

    Update

--- app/libs/SudokuSolver/app/build.gradle.kts
+++ app/libs/SudokuSolver/app/build.gradle.kts
@@ -39,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -48,6 +47,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+    implementation("com.google.mlkit:text-recognition:16.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/libs/SudokuSolver/app/src/main/java/com/antoniopelusi/sudokusolver/ui/camera/CameraFragment.kt
+++ app/libs/SudokuSolver/app/src/main/java/com/antoniopelusi/sudokusolver/ui/camera/CameraFragment.kt
@@ -6,20 +6,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Parcelable
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import android.widget.Toast.makeText
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
-import androidx.core.view.drawToBitmap
 import androidx.fragment.app.Fragment
 import com.antoniopelusi.sudokusolver.databinding.FragmentCameraBinding
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class CameraFragment : Fragment() {
 
@@ -32,26 +34,66 @@ class CameraFragment : Fragment() {
     private val REQUEST_CODE = 200
 
     private lateinit var imageView: ImageView
-    private lateinit var button: Button
+    private lateinit var cameraButton: Button
+    private lateinit var solveButton: Button
 
-    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-            isGranted: Boolean -> if (isGranted){
-        // Permission Accepted
-        makeText(activity, "Permission Accepted.", LENGTH_SHORT).show()
-    }
-    else {
-        // Permission Denied
-        makeText(activity, "Permission Denied.", LENGTH_SHORT).show()
-    }
+    private lateinit var bitmap: Bitmap
+
+    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission())
+    {
+        isGranted: Boolean -> if (isGranted)
+        {
+            // Permission Accepted
+                makeText(activity, "Permission Accepted.", LENGTH_SHORT).show()
+        }
+        else {
+            // Permission Denied
+            makeText(activity, "Permission Denied.", LENGTH_SHORT).show()
+        }
     }
 
-    private fun capturePhoto() {
+    private fun capturePhoto()
+    {
         if(ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
         {
             val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(cameraIntent, REQUEST_CODE)
         }
         else makeText(activity, "Camera is necessary to add content.", LENGTH_SHORT).show()
+    }
+
+    private fun ocr()
+    {
+        val image = InputImage.fromBitmap(bitmap, 0)
+
+        var out = ""
+
+        val result = recognizer.process(image)
+            .addOnSuccessListener { visionText ->
+                for (block in visionText.textBlocks) {
+                    val boundingBox = block.boundingBox
+                    val cornerPoints = block.cornerPoints
+                    val text = block.text
+
+                    for (line in block.lines) {
+                        for (element in line.elements) {
+                            for(symbol in element.symbols)
+                            {
+
+                            }
+                        }
+                    }
+
+                    makeText(context, visionText.text, LENGTH_SHORT).show()
+
+                }
+            }
+            .addOnFailureListener { e ->
+                makeText(context, e.toString(), Toast.LENGTH_SHORT).show()
+            }
+
 
     }
 
@@ -82,9 +124,14 @@ class CameraFragment : Fragment() {
 
         imageView = binding.clickImage
 
-        button = binding.cameraButton
-        button.setOnClickListener {
+        cameraButton = binding.cameraButton
+        cameraButton.setOnClickListener {
             capturePhoto()
+        }
+
+        solveButton = binding.solveButtonCamera
+        solveButton.setOnClickListener {
+            ocr()
         }
 
         return root
@@ -92,8 +139,10 @@ class CameraFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
-            imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null)
+        {
+            bitmap = data.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bitmap)
         }
     }
 
