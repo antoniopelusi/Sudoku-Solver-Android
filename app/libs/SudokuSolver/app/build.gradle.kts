d1903ddc12d0d20cb99050982cd34af027f253a2
antoniopelusi <antoniopelusi2000@gmail.com>
2023-10-12 09:46:51 +0200

    Update

--- app/libs/SudokuSolver/app/build.gradle.kts
+++ app/libs/SudokuSolver/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.antoniopelusi.sudokusolver"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
